bf20ef4d3e87c6015ae9dadfe4e105e5a0b98d2e
Anderson Karl <42244540+Artoshk@users.noreply.github.com>
2024-07-19 21:28:14 -0300

    whisperx init and tests

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import ffmpeg
+
+def load_audio(file_bytes: bytes, sr: int = 16_000) -> np.ndarray:
+    """
+    Use file's bytes and transform to mono waveform, resampling as necessary
+    Parameters
+    ----------
+    file: bytes
+        The bytes of the audio file
+    sr: int
+        The sample rate to resample the audio if necessary
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input('pipe:', threads=0)
+            .output("pipe:", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run_async(pipe_stdin=True, pipe_stdout=True)
+        ).communicate(input=file_bytes)
+
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
--- tests/tests.py
+++ tests/tests.py
@@ -4,56 +4,68 @@ from main import app  # Import your FastAPI app
 # Initialize TestClient
 client = TestClient(app)
 
-def test_process_audio():
-    # Load a sample audio file for testing
+def should_process_audio():
+    # Arrange
     with open("tests/audio_pass.wav", "rb") as audio_file:
+        # Act
         response = client.post("/process_audio", files={"audio": audio_file})
-    
+    # Assert
     assert response.status_code == 200
     result = response.json()
     assert result["result"] == " 25 vezes 35 dividido por 2.\n\n25*35/2 = 437.5"
     
-def test_process_audio_invalid():
-    # Load a sample audio file for testing
+def should_not_process_invalid_audio():
+    # Arrange
     with open("tests/audio_fail.wav", "rb") as audio_file:
-        response = client.post("/process_audio", files={"audio": audio_file})
-    
+        # Act
+        response = client.post("/process_audio", files={"audio": audio_file}) 
+    # Assert
     assert response.status_code == 200
     result = response.json()
     assert "Operação não suportada" in result["result"]
     
-def test_process_audio_unsupported_operation():
-    # Load a sample audio file for testing
+def should_not_process_files_that_are_not_audio():
+    # Arrange
     with open("tests/not_audio_file.wav", "rb") as audio_file:
+        # Act
         response = client.post("/process_audio", files={"audio": audio_file})
-    
+    # Assert
     assert response.status_code == 422
     result = response.json()
     assert "Operação não suportada" in result['detail']
 
-def test_process_equation_valid():
+def should_add_numbers():
+    # Arrange
     form_data = {
         'equation': '2 + 2'
     }
+    # Act
     response = client.post("/process_equation", data=form_data)
+    # Assert
     assert response.status_code == 200
     result = response.json()
     assert result["result"] == "2 + 2 = 4"
 
-def test_process_equation_invalid():
+def should_not_divide_by_zero():
+    # Arrange
     form_data = {
         'equation': '2 / 0'
     }
+    # Act
     response = client.post("/process_equation", data=form_data)
+    # Assert
     assert response.status_code == 422
     result = response.json()
     assert result["detail"] == "division by zero"
 
-def test_process_equation_unsupported_operation():
+def should_not_process_invalid_equation():
+    # Arrange
     form_data = {
         'equation': 'unsupported operation'
     }
+    # Act
     response = client.post("/process_equation", data=form_data)
+    # Assert
     assert response.status_code == 422
     result = response.json()
     assert "invalid syntax" in result["detail"]
